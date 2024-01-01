4f6a17c65a072bdc24036efa96096efd80e52400
AbdulAhad-150 <abdulahad150@gmail.com>
2023-12-31 21:21:55 -0800

    Sayalani form

---
+++ app.js
@@ -0,0 +1,10 @@
+var butt = document.getElementById("but");
+var nam = document.getElementById("name");
+
+function submitButton() {
+  if (nam.value == "") {
+      alert("filled the details");
+    } else {
+    alert(" successful");
+  }
+}
